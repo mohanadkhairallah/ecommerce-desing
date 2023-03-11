93d6abbe53cb8dd3c6647d26b41b772860b2ae33
Mohanad khairallah <mohanad.khairallah95@gmail.com>
2023-03-11 03:00:31 +0300

    init commit

--- lib/app/global_controllers.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:get/get.dart';
-//
-// import '../controllers/app_controller.dart';
-// import '../controllers/navigation_controller.dart';
-//
-// NavigationController navigationController = Get.find();
-// AppController appController = Get.find();
